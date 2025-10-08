035c841a7a76b1f37c6f5487ce0d35603f4484e2
de1vos <66249627+de1vos@users.noreply.github.com>
2025-10-08 09:25:24 +0200

    vga testing, resolution update

--- Day3_Test/hardware_addresses.h
+++ Day3_Test/hardware_addresses.h
@@ -1,5 +1,5 @@
 // DTEK-V harware addresses
-#define VGA_FRAMEBUFFER     0x08000000 //writing to this address means writing to the screen
+#define VGA_FRAMEBUFFER     0x08000000 //writing to this address means writing to the screen, 320 x 240 resolution
 #define VGA_PIXELBUFFER_DMA 0x04000100 //handels transfer between memory and vga output
 #define BUTTON_BASE         0x040000d0 //address to button on DE10-Lite board
 #define SWITCH_BASE         0x04000010 //Address of DE10-Lite switch state
